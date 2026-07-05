6f39194a3f9d7134051e6e5035cfedae207cb278
mayo78 <75394229+mayo78@users.noreply.github.com>
2026-07-05 00:02:41 -0400

    fix wholesomer scale

--- project/src/backend/sdl/SDLApplication.cpp
+++ project/src/backend/sdl/SDLApplication.cpp
@@ -264,7 +264,7 @@ namespace lime {
 			case SDL_EVENT_WINDOW_MOVED:
 			case SDL_EVENT_WINDOW_RESTORED:
 			case SDL_EVENT_WINDOW_EXPOSED:
-			case SDL_EVENT_WINDOW_RESIZED:
+			case SDL_EVENT_WINDOW_PIXEL_SIZE_CHANGED:
 
 				ProcessWindowEvent(event);
 				break;
